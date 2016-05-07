9794690229b1802c2ffc790f509175ba08b5c4d8
MwumLi <mwumli@hotmail.com>
2016-05-07 15:11:38 +0800

    add tmux

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dotfiles
 Sync tmux, cheat, shell ...
+
+## Configurations
+
+* cheat : `cheat` 命令的 `cheatsheet`  
+  Usage : `mkdir -p ~/.cheat ; cp -Rv cheat/* ~/.cheat/`
+
+* tmux : `tmux` 的配置  
+  Usage : `cd tmux && sh gen.sh && cp tmux.conf ~/.tmux.conf`
---
+++ tmux/gen.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+cat tmux.basic.conf > tmux.conf
+
+tmux_version_major=`tmux -V | cut -d' ' -f2 | cut -d'.' -f1`
+tmux_version_minor=`tmux -V | cut -d' ' -f2 | cut -d'.' -f2`
+
+if [ $tmux_version_major -eq 2 -a $tmux_version_minor -lt 1 ] || [ $tmux_version_major -le 1 ]
+then
+    cat tmux.mouse1.conf >> tmux.conf
+else
+    cat tmux.mouse2.conf >> tmux.conf
+fi
